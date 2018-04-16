676786392cba7b546eee12f9570541d74aaa8946
leni le goff <lenilegoff@gmail.com>
2018-04-16 14:23:46 +0200

    add rgbd_utils relevance_map package

--- include/relevance_map/parameters.hpp
+++ include/relevance_map/parameters.hpp
@@ -3,14 +3,19 @@
 
 namespace  relevance_map {
 
+/**
+ * @brief VCCS hyperparameters and camera intrisic parameters
+ * This parameters are used as default for the image_processing library
+ */
 struct sv_param {
+    //Supervoxels hyperparameters
     static constexpr bool use_transform = false;
     static constexpr double voxel_resolution = 0.008f;
     static constexpr double color_importance = 0.2f;
     static constexpr double spatial_importance = 0.4f;
     static constexpr double normal_importance = 0.4f;
     static constexpr double seed_resolution = 0.05f;
-    //CAMERA PARAMETERS
+    //Camera intrisic parameters
     static constexpr float depth_princ_pt_x = 479.75;
     static constexpr float depth_princ_pt_y = 269.75;
     static constexpr float rgb_princ_pt_x = 479.75;
--- include/relevance_map/score_computation.hpp
+++ include/relevance_map/score_computation.hpp
@@ -4,21 +4,32 @@
 #include <tbb/tbb.h>
 #include <image_processing/SurfaceOfInterest.h>
 #include <relevance_map/utilities.hpp>
+#include <chrono>
 
 namespace relevance_map {
 
 template <class Node>
 class score_computation{
     public:
+
+        std::vector<std::string> statistics_name;
+
         score_computation(Node* node, std::string output_file) :
             _node(node), _output_file(output_file){
-            _weights = _node->get_soi().get_weights()[_node->get_modality()];
-            for(const auto& w : _weights)
-                _lbls.push_back(w.first);
+            init();
         }
         score_computation(score_computation& sc, tbb::split) :
             _node(sc._node), _weights(sc._weights), _lbls(sc._lbls),
             _tp(0), _tn(0), _fp(0), _fn(0), _total_neg(0), _total_pos(0){}
+
+
+        void init(){
+            _lbls.clear();
+            _weights.clear();
+            _weights = _node->get_soi().get_weights()[_node->get_modality()];
+            for(const auto& w : _weights)
+                _lbls.push_back(w.first);
+        }
 
         void operator ()(const tbb::blocked_range<size_t>& r){
             double tp = _tp, fp = _fp, fn = _fn, tn = _tn, total_neg = _total_neg, total_pos = _total_pos;
@@ -63,120 +74,118 @@ class score_computation{
         double _total_neg = 0;
         double _total_pos = 0;
 
-    void compute_scores_results(int iter, int nb_false_pos = 0, int nb_false_neg = 0){
-        std::chrono::system_clock::time_point timer;
-        timer  = std::chrono::system_clock::now();
-        //* Compute scores of performance
-        double precision, recall, accuracy;
-        compute_precision_recall(precision,recall,accuracy);
+        void compute_scores_results(int iter, int nb_false_pos = 0, int nb_false_neg = 0){
+            std::chrono::system_clock::time_point timer;
+            timer  = std::chrono::system_clock::now();
+            statistics_name = {"nb_samples","precision","recall","accuracy",
+                               "pos_samples","neg_samples",
+                               "pos_components","neg_components",
+                               "false_positives","false_negatives",
+                               "rand_nb_pos","rand_nb_pos"};
+            //* Compute scores of performance
+            double precision, recall, accuracy;
+            compute_precision_recall(precision,recall,accuracy);
 
-        int nb_samples, nb_neg, nb_pos, nb_pos_comp, nb_neg_comp,rand_nb_pos, rand_nb_neg;
-        if(_node->get_method() == "nnmap"){
-            nb_samples = _node->_nnmap_class[_node->get_modality()].get_samples().size();
-            nb_neg = _node->_nnmap_class[_node->get_modality()].get_samples().get_data(0).size();
-            nb_pos = _node->_nnmap_class[_node->get_modality()].get_samples().get_data(1).size();
-            nb_pos_comp = 0;
-            nb_neg_comp = 0;
+            int nb_samples, nb_neg, nb_pos, nb_pos_comp, nb_neg_comp,rand_nb_pos, rand_nb_neg;
+            if(_node->get_method() == "nnmap"){
+                nb_samples = _node->_nnmap_class[_node->get_modality()].get_samples().size();
+                nb_neg = _node->_nnmap_class[_node->get_modality()].get_samples().get_data(0).size();
+                nb_pos = _node->_nnmap_class[_node->get_modality()].get_samples().get_data(1).size();
+                nb_pos_comp = 0;
+                nb_neg_comp = 0;
+            }
+            else if(_node->get_method() == "gmm"){
+                nb_samples = _node->_gmm_class[_node->get_modality()].get_samples().size();
+                nb_neg = _node->_gmm_class[_node->get_modality()].get_samples().get_data(0).size();
+                nb_pos = _node->_gmm_class[_node->get_modality()].get_samples().get_data(1).size();
+                nb_pos_comp = _node->_gmm_class[_node->get_modality()].model()[1].size();
+                nb_neg_comp = _node->_gmm_class[_node->get_modality()].model()[0].size();
+            }
+            else if(_node->get_method() == "mcs"){
+                nb_samples = _node->_mcs.get_nb_samples();
+                nb_neg = _node->_mcs.get_samples().get_data(0).size();
+                nb_pos = _node->_mcs.get_samples().get_data(1).size();
+                nb_pos_comp = 0;
+                nb_neg_comp = 0;
+            }
+            rand_nb_pos = nb_samples*_total_pos/(_total_pos+_total_neg);
+            rand_nb_neg = nb_samples - rand_nb_pos;
+
+            ROS_INFO_STREAM("--------------------------------------------------------");
+            ROS_INFO_STREAM("scores for iteration " << iter << "\n"
+                            << " precision : " << precision << "\n"
+                            << " recall : " << recall << "\n"
+                            << " accuracy : " << accuracy << "\n"
+                            << " nb components : pos " << nb_pos_comp << "; neg " << nb_neg_comp << "\n"
+                            << " nb samples : pos " << nb_pos << "; neg " << nb_neg << "\n"
+                            << " false samples : pos " << nb_false_pos << "; neg " << nb_false_neg << "\n"
+                            << " rand nb : pos " << rand_nb_pos << " ; neg " << rand_nb_neg);
+            ROS_INFO_STREAM("--------------------------------------------------------");
+
+            std::stringstream str;
+            str << "iteration_" << iter;
+            set_results(str.str(),{nb_samples,
+                                                 precision,
+                                                 recall,
+                                                 accuracy,
+                                                 nb_pos,
+                                                 nb_neg,
+                                                 nb_pos_comp,
+                                                 nb_neg_comp,
+                                                 nb_false_pos,
+                                                 nb_false_neg,
+                                                 rand_nb_pos,
+                                                 rand_nb_neg});
+
+            if(!write_result(_output_file))
+                ROS_ERROR_STREAM("unable to open " << _output_file);
+
+            ROS_INFO_STREAM("Computing scores pra finish, time spent : "
+                            << std::chrono::duration_cast<std::chrono::milliseconds>(
+                                std::chrono::system_clock::now() - timer).count());
+
         }
-        else if(_node->get_method() == "gmm"){
-            nb_samples = _node->_gmm_class[_node->get_modality()].get_samples().size();
-            nb_neg = _node->_gmm_class[_node->get_modality()].get_samples().get_data(0).size();
-            nb_pos = _node->_gmm_class[_node->get_modality()].get_samples().get_data(1).size();
-            nb_pos_comp = _node->_gmm_class[_node->get_modality()].model()[1].size();
-            nb_neg_comp = _node->_gmm_class[_node->get_modality()].model()[0].size();
+
+        void compute_precision_recall(double& precision, double& recall, double& accuracy){
+            ROS_INFO_STREAM("compute precision, recall and accuracy");
+            precision = 0; recall = 0; accuracy = 0;
+
+            tbb::parallel_reduce(tbb::blocked_range<size_t>(0,_node->get_soi().get_weights()[_node->get_modality()].size()),*this);
+
+
+            precision = _tp / (_tp + _fp);
+            recall = _tp / (_tp + _fn);
+            accuracy = (_tp/_total_pos + _tn/_total_neg) / 2.;
         }
-        else if(_node->get_method() == "mcs"){
-            nb_samples = _node->_mcs.get_nb_samples();
-            nb_neg = _node->_mcs.get_samples().get_data(0).size();
-            nb_pos = _node->_mcs.get_samples().get_data(1).size();
-            nb_pos_comp = 0;
-            nb_neg_comp = 0;
+
+        int write_result(std::string file_name){
+            ROS_INFO_STREAM("start to write output file");
+            std::ofstream ofs(file_name,std::ofstream::out | std::ofstream::app);
+            if(!ofs.is_open())
+                return 0;
+
+            YAML::Emitter emitter;
+            emitter << YAML::BeginMap;
+            emitter << YAML::Key << _result.first << YAML::Value
+                    << YAML::BeginMap;
+            for(int i = 0; i < statistics_name.size(); i++){
+                emitter << YAML::Key << statistics_name[i] << YAML::Value << _result.second[i];
+            }
+
+            emitter << YAML::EndMap;
+            emitter << YAML::EndMap;
+
+            ofs << emitter.c_str();
+            ofs << "\n";
+            ofs.close();
+            return 1;
         }
-        rand_nb_pos = nb_samples*_total_pos/(_total_pos+_total_neg);
-        rand_nb_neg = nb_samples - rand_nb_pos;
 
-        ROS_INFO_STREAM("--------------------------------------------------------");
-        ROS_INFO_STREAM("scores for iteration " << iter << "\n"
-                        << " precision : " << precision << "\n"
-                        << " recall : " << recall << "\n"
-                        << " accuracy : " << accuracy << "\n"
-                        << " nb components : pos " << nb_pos_comp << "; neg " << nb_neg_comp << "\n"
-                        << " nb samples : pos " << nb_pos << "; neg " << nb_neg << "\n"
-                        << " false samples : pos " << nb_false_pos << "; neg " << nb_false_neg << "\n"
-                        << " rand nb : pos " << rand_nb_pos << " ; neg " << rand_nb_neg);
-        ROS_INFO_STREAM("--------------------------------------------------------");
+        void set_results(const std::string& label,const result_array_t& value){
+            _result = std::make_pair(label,value);
+        }
 
-        std::stringstream str;
-        str << "iteration_" << iter;
-        _result = std::make_pair(str.str(),
-                         result_array_t{{nb_samples,
-                                               precision,
-                                               recall,
-                                               accuracy,
-                                               nb_pos,
-                                               nb_neg,
-                                               nb_pos_comp,
-                                               nb_neg_comp,
-                                               nb_false_pos,
-                                               nb_false_neg,
-                                               rand_nb_pos,
-                                               rand_nb_neg}});
-
-        if(!write_result(_output_file))
-            ROS_ERROR_STREAM("unable to open " << _output_file);
-
-        ROS_INFO_STREAM("Computing scores pra finish, time spent : "
-                        << std::chrono::duration_cast<std::chrono::milliseconds>(
-                            std::chrono::system_clock::now() - timer).count());
-
-    }
-
-    void compute_precision_recall(double& precision, double& recall, double& accuracy){
-        ROS_INFO_STREAM("compute precision, recall and accuracy");
-        precision = 0; recall = 0; accuracy = 0;
-
-        tbb::parallel_reduce(tbb::blocked_range<size_t>(0,_node->get_soi().get_weights()[_node->get_modality()].size()),*this);
-
-
-        precision = _tp / (_tp + _fp);
-        recall = _tp / (_tp + _fn);
-        accuracy = (_tp/_total_pos + _tn/_total_neg) / 2.;
-    }
-
-    int write_result(std::string file_name){
-        ROS_INFO_STREAM("start to write output file");
-        std::ofstream ofs(file_name,std::ofstream::out | std::ofstream::app);
-        if(!ofs.is_open())
-            return 0;
-
-        YAML::Emitter emitter;
-        emitter << YAML::BeginMap;
-        emitter << YAML::Key << _result.first << YAML::Value
-                << YAML::BeginMap
-                << YAML::Key << "nbr_samples" << YAML::Value << ((int)_result.second[0])
-                << YAML::Key << "precision" << YAML::Value << _result.second[1]
-                << YAML::Key << "recall" << YAML::Value << _result.second[2]
-                << YAML::Key << "accuracy" << YAML::Value << _result.second[3]
-                << YAML::Key << "pos_samples" << YAML::Value << _result.second[4]
-                << YAML::Key << "neg_samples" << YAML::Value << _result.second[5]
-                << YAML::Key << "pos_components" << YAML::Value << _result.second[6]
-                << YAML::Key << "neg_components" << YAML::Value << _result.second[7]
-                << YAML::Key << "false_positives" << YAML::Value << _result.second[8]
-                << YAML::Key << "false_negatives" << YAML::Value << _result.second[9]
-                << YAML::Key << "rand_nb_pos" << YAML::Value << _result.second[10]
-                << YAML::Key << "rand_nb_neg" << YAML::Value << _result.second[11]
-                << YAML::EndMap;
-
-
-        emitter << YAML::EndMap;
-
-        ofs << emitter.c_str();
-        ofs << "\n";
-        ofs.close();
-        return 1;
-    }
-
-    private:
+private:
         Node* _node;
         ip::SurfaceOfInterest::saliency_map_t _weights;
         std::vector<uint32_t> _lbls;
